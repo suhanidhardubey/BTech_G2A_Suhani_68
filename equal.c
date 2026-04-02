422fd611c6523bf1f9b13decf4205f53f3b0354b
suhanidhardubey <suhanidhardubey@gmail.com>
2026-04-02 15:47:52 +0530

    Add program to check if two numbers are equal

---
+++ equal.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter numbera a :");
+    scanf("%d",&a);
+    printf("Enter numbera b :");
+    scanf("%d",&b);
+    if (a==b){
+        printf("Given number are equal");
+    }
+    else {
+    printf("Given number are not equal");
+    }
+    return 0;
+}
