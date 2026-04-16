7954aa93f9a78bc8fe1424bb8eaab04aca6b13c5
suhanidhardubey <suhanidhardubey@gmail.com>
2026-04-16 09:42:04 +0530

    Add swap function using pointers in C

---
+++ Swap using pointers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// Function to swap two numbers using pointers
+void swap(int *a, int *b) {
+    int temp;
+    temp = *a;   // store value of a
+    *a = *b;     // assign b to a
+    *b = temp;   // assign temp to b
+}
+
+int main() {
+    int x, y;
+
+    printf("Enter two numbers:\n");
+    scanf("%d %d", &x, &y);
+
+    printf("Before swapping: x = %d, y = %d\n", x, y);
+
+    // Passing addresses of x and y
+    swap(&x, &y);
+
+    printf("After swapping: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
